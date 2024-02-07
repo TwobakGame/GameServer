3a963562c7a90d8cc4906e5f81a9cbc20c6a8d02
KJY-kpu <rntlls1366@gmail.com>
2024-02-07 14:38:14 +0900

    ⚡️	: 입장불가능한 방에 대한 관리 강화
    ################
    #🎨	: 코드의 구조/형태 개선
    #⚡️	: 성능 개선
    #🔥	: 코드/파일 삭제
    #🐛	: 버그 수정
    #🚑	: 긴급 수정
    #✨	: 새 기능
    #📝	: 문서 추가/수정
    #💄	: UI/스타일 파일 추가/수정
    #🎉	: 프로젝트 시작
    #✅	: 테스트 추가/수정
    #🔒	: 보안 이슈 수정
    #🔖	: 릴리즈/버전 태그
    #💚	: CI 빌드 수정
    #📌	: 특정 버전 의존성 고정
    #👷	: CI 빌드 시스템 추가/수정
    #📈	: 분석, 추적 코드 추가/수정
    #♻️	: 코드 리팩토링
    #➕	: 의존성 추가
    #➖	: 의존성 제거
    #🔧	: 구성 파일 추가/삭제
    #🔨	: 개발 스크립트 추가/수정
    #🌐	: 국제화/현지화
    #💩	: 똥싼 코드
    #⏪	: 변경 내용 되돌리기
    #🔀	: 브랜치 합병
    #📦	: 컴파일된 파일 추가/수정
    #👽	: 외부 API 변화로 인한 수정
    #🚚	: 리소스 이동, 이름 변경
    #📄	: 라이센스 추가/수정
    #💡	: 주석 추가/수정
    #🍻	: 술 취해서 쓴 코드
    #🗃	: 데이터베이스 관련 수정
    #🔊	: 로그 추가/수정
    #🙈	: .gitignore 추가/수정
    ################

--- server.js
+++ server.js
@@ -19,9 +19,28 @@ wsServer.on('connection', (socket) => {
     socket.onAny((event) => {
         console.log(`이벤트 발생 : ${event}`);
     });
-    socket.on("join", (room) => {
-        socket.join(room);
-        socket["roomcode"] = room;
+    socket.on("join", (room, type) => {
+        const roomSize = wsServer.sockets.adapter.rooms.get(room)?.size || 0;
+        console.log(room, " 인원수 : " , roomSize);
+        if(type === "manager") {
+            if(roomSize === 0) {
+                socket.join(room);
+                socket["roomcode"] = room;
+            }
+            else {
+                socket.emit("error", "올바르지 않은 방 생성입니다. 다시 시도해주세요.");
+            }
+        }
+        else if(type === "client") {
+            if(roomSize === 1) {
+                socket.join(room);
+                socket["roomcode"] = room;
+            }
+            else {
+                socket.emit("error", "입장 불가능한 방 번호입니다.");
+            }
+        }
+        
     });
     socket.on("offer", (offer) => {
         socket.to(socket.roomcode).emit("offer", offer);
